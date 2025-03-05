de979a72e58742cbc5bd790849525591cf9fd103
Claaarree <clare.fh@hotmail.com>
2025-03-05 11:56:22 +0800

    removed api key

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -9,11 +9,11 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient) { }
 
-  baseUrl: string = 'http://api.weatherapi.com/v1/current.json'
+  baseUrl: string = 'https://api.weatherapi.com/v1/current.json'
 
   getWeatherDetails(city: string): Observable<any> {
     const params = new HttpParams()
-      .append('key', 'db403e8af1aa4ff296325345240612')
+      .append('key', 'enterAPIKey to work')
       .append('q', city)
       .append('aqi', 'no');
 
